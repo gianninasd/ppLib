3c222207b33fd9c8995c475491cff13aaa9976b7
Jimmy G <angelone55@hotmail.com>
2020-03-20 13:47:46 -0400

    added logging

--- src/impl/PaysafePaymentService.php
+++ src/impl/PaysafePaymentService.php
@@ -18,6 +18,7 @@ class PaysafePaymentService implements PaymentService {
       error_log($request->uuid . " Sending payment for " . $request->id);
 
       $ch = curl_init();
+      error_log("0011>");
       curl_setopt($ch, CURLOPT_URL, $request->url);
       curl_setopt($ch, CURLOPT_POST, true);
       curl_setopt($ch, CURLOPT_POSTFIELDS, $request->body);
@@ -26,13 +27,19 @@ class PaysafePaymentService implements PaymentService {
       curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
       curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json', 'Authorization: Basic ' . $request->authenticationToken) );
 
+      error_log("11>");
+
       $resp = new PaymentResponse();
       $resp->uuid = $request->uuid;
       $resp->body = curl_exec($ch);
       $resp->curlErrorNo = curl_errno($ch);
       $resp->curlErrorDetails = curl_error($ch);
       $resp->httpResponseCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+      error_log($resp->uuid . " Response(" . $resp->httpResponseCode . "): " . $resp->curlErrorNo . " (" . $resp->curlErrorDetails . ") - body: " . $resp->body);
+      error_log("22>");
       curl_close($ch);
+      error_log("33>");
 
       error_log($resp);
 
